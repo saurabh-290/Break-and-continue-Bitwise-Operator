6f0f782f7fdae597041a2ad62cb3c31cd9622c2b
saurabh-290 <132060392+saurabh-290@users.noreply.github.com>
2024-01-06 23:06:21 +0530

    Add files via upload

---
+++ 3rd.cpp
@@ -0,0 +1,152 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // cout <<"Bitwise And Operators" <<endl;
+    //  cout<<"___________________________________"<<endl;
+
+
+    // int a= true;
+    // int b= false;
+
+    // cout << ( a & b ) << endl;
+
+
+
+
+
+
+//  cout <<"Bitwise OR Operators" <<endl;
+//  cout<<"___________________________________"<<endl;
+
+
+//     int a= true;
+//     int b= false;
+    
+//     cout << ( a || b ) << endl;
+
+
+
+
+
+//  cout <<"Bitwise XOR Operators" <<endl;
+//  cout<<"___________________________________"<<endl;
+
+
+//     int a= true;
+//     int b= false;
+    
+//     cout << ( a ^ b ) << endl;
+
+
+
+
+
+// cout <<"Bitwise NOT Operators" <<endl;
+//  cout<<"___________________________________"<<endl;
+
+
+//     int a= true;
+    
+    
+//     cout << ~( a ) << endl;
+
+
+
+
+
+// cout <<"Left Shift Operator " <<endl;
+//  cout<<"___________________________________"<<endl;
+
+
+//     int a= 12;
+//     a= a<<1;
+    
+//     cout << a << endl;
+
+
+
+
+
+// cout <<"Right Shift Operator " <<endl;
+//  cout<<"___________________________________"<<endl;
+
+
+    // int a= 12;
+    // a= a >> 1;
+    
+    // cout << a << endl;
+
+
+
+
+
+    // cout <<"Pre increment  "<<endl;
+    //  cout<<"___________________________________"<<endl;
+   
+    // int a=5;
+    // cout << ++a <<endl;
+
+
+
+
+
+
+    // cout <<"Post increment  "<<endl;
+    //  cout<<"___________________________________"<<endl;
+    
+      // int a=10;
+    // cout << a++ <<endl;
+
+
+
+    // cout <<"Pre decrement  "<<endl;
+    //  cout<<"___________________________________"<<endl;
+
+    // int a=5;
+    // cout << --a <<endl;
+
+
+
+
+
+    // cout <<"Post decrement "<<endl;
+    //  cout<<"___________________________________"<<endl;
+
+    // int a=10;
+    // cout << a-- <<endl;
+
+
+
+
+
+    // cout <<"Break "<<endl;
+    //  cout<<"___________________________________"<<endl;
+
+    // for(int i=0; i<5; i++)
+    // {  
+    //     cout << i <<endl;
+    //     break;
+
+    // }
+
+
+
+
+    cout <<"Continue"<<endl;
+    cout<<"___________________________________"<<endl;
+
+    for(int i=0; i<5; i++)
+    {
+        
+        continue;
+        cout << i <<endl;
+
+        
+
+    }
+    
+
+
+return 0;
+}
